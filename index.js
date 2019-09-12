418662e135a6739cf837811f5e2ab69a6e467236
ntnyq <ntnyq13@gmail.com>
2019-09-12 23:22:56 +0800

    fix: allow template literals

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 module.exports = {
-  extends: [
-    'standard'
-  ],
+  extends: ['standard'],
 
   rules: {
+    quotes: [2, 'single', { allowTemplateLiterals: true }],
     'comma-dangle': [2, 'always-multiline']
   },
 }
